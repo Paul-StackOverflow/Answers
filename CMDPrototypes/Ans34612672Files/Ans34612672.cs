54e683df8a5cea79671fac8f5de4e67c52cc30bb
Paul Callaghan - DevCreek <developer4102@roadrunner.com>
2016-01-23 08:51:50 -0500

    Answer about a score manager concept.

---
+++ CMDPrototypes/Ans34612672Files/Ans34612672.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CMDPrototypes.Ans34612672Files;
+
+namespace CMDPrototypes
+{
+    public class Ans34612672
+    {
+        public Ans34612672()
+        {
+
+        }
+
+        public bool Execute()
+        {
+            bool retValue = false;
+            scoremanager Manager = new scoremanager();
+            Manager.Start();
+            while(Manager.score < 20)
+            {
+                Manager.Update();
+            }
+            Console.WriteLine(Manager.distance);
+            Console.ReadKey();
+            return retValue;
+        }
+
+    }
+}
