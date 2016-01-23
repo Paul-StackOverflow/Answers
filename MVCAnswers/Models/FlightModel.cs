c2341a0226b4b20857360f25153ff8c5cbfb6eee
Paul Callaghan - DevCreek <developer4102@roadrunner.com>
2016-01-23 08:54:29 -0500

    used in Ans34121398 controller.

--- MVCAnswers/Models/FlightModel.cs
+++ MVCAnswers/Models/FlightModel.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using System.Linq;
 using System.ComponentModel.DataAnnotations;
 
 namespace MVCAnswers
