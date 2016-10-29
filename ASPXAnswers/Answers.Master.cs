26893427790efc331a463ac47b5dbf4ac79d8a09
Paul Callaghan <developer4102@outlook.com>
2016-10-29 09:31:26 -0400

    Question 40318489

--- ASPXAnswers/Answers.Master.cs
+++ ASPXAnswers/Answers.Master.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
+using System.Web.UI.HtmlControls;
 using System.Web.UI.WebControls;
+using System.Linq;
 
 namespace ASPXAnswers
 {
@@ -11,6 +13,10 @@ namespace ASPXAnswers
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            var controls = this.head.Parent.Controls;
+            var stuffWrapper = this.head.Parent.Controls[0];//("og:image");  http://stackoverflow.com/questions/40318489/read-the-tag-content-of-open-graph-from-code-behind-with-c-sharp/40319364#40319364
+            var stuff = ((System.Web.UI.HtmlControls.HtmlMeta)stuffWrapper).Content;
+            var stop = stuff;
             //string TargetPage = Page.AppRelativeVirtualPath;
             //if (TargetPage.ToLower().Contains("one"))
             //{
